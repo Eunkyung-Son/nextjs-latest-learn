43921a6420934364670368501461cfa112e2ceb4
Eunkyung-Son <sek9510@naver.com>
2023-06-19 10:34:20 +0900

    [FIX] time ago ui position

--- src/app/isr/[id]/page.tsx
+++ src/app/isr/[id]/page.tsx
@@ -17,15 +17,15 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <div className="grid grid-cols-6 gap-x-6 gap-y-3">
+      <div className="-order-1 col-span-full lg:order-none lg:col-span-2">
+        <RenderingInfo type="isr" />
+      </div>
       <div className="col-span-full space-y-3 lg:col-span-4">
         <h1 className="truncate text-2xl font-medium capitalize text-gray-200">
           {/* 타이틀{JSON.stringify(data)} */}
           {params.id}
         </h1>
         <p className="font-medium text-gray-500">{JSON.stringify(data)}</p>
-      </div>
-      <div className="-order-1 col-span-full lg:order-none lg:col-span-2">
-        <RenderingInfo type="isr" />
       </div>
     </div>
   );
